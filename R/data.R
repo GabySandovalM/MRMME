b8768af81391c4bda072f18fa4f3570e53410bce
Gabriela Sandoval <fgsandoval@uc.cl>
2022-07-01 04:50:08 -0400

    document the dataset

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Human lung capacity data
+#'
+#' The data set reported in Barnett (1969) involves different instruments for measuring human
+#' lung capacity. These data were collected as part of a medical study to compare the relative merits of
+#' two instruments designed to measure human lung function-specific aspects. One of the two
+#' instruments is the standard, and the other is a new instrument, which, among other features,
+#' requires less skill in its operation. So, the study considers two operators, one skilled and one
+#' unskilled, to take the measurements on 72 patients.
+#'
+#' @format A tibble with 72 rows and 4 columns:
+#' \describe{
+#'   \item{X1}{The vital capacity mesured by the skilled operator with the instrument I (standard).}
+#'   \item{Y1}{The vital capacity mesured by the unskilled operator with the instrument I(standard).}
+#'   \item{X2}{The vital capacity mesured by the skilled operator with the instrument II (new).}
+#'   \item{Y2}{The vital capacity mesured by the unskilled operator with the instrument II (new).}
+#' }
+#' @references
+#'
+"lung"
+
